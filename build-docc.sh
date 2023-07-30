528694a112976d2acbf9a36508210c78869d46fd
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-07-30 22:00:10 +0200

    fix: derived path

--- build-docc.sh
+++ build-docc.sh
@@ -3,9 +3,9 @@
 xcrun xcodebuild docbuild \
     -scheme AccessibleSpatialComputing \
     -destination 'generic/platform=macOS' \
-    -derivedDataPath "~/Desktop/Docs"
+    -derivedDataPath "$PWD/.derivedData"
 
 xcrun docc process-archive transform-for-static-hosting \
-    "~/Desktop/Docs/Build/Products/Debug/A11Y-SC-NYC.doccarchive" \
+    "$PWD/.derivedData/Build/Products/Debug/A11Y-SC-NYC.doccarchive" \
     --output-path ".docs" \
     --hosting-base-path "accessible-spatial-computing"
