d6525d3f0c53f63a8f55eebc4d2878453ed50fa8
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-07-30 22:03:28 +0200

    ci: try building with iOS

--- build-docc.sh
+++ build-docc.sh
@@ -2,10 +2,10 @@
 
 xcrun xcodebuild docbuild \
     -scheme AccessibleSpatialComputing \
-    -destination 'generic/platform=macOS' \
+    -destination 'generic/platform=iOS' \
     -derivedDataPath "$PWD/.derivedData"
 
 xcrun docc process-archive transform-for-static-hosting \
-    "$PWD/.derivedData/Build/Products/Debug/A11Y-SC-NYC.doccarchive" \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/A11Y-SC-NYC.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "accessible-spatial-computing"
+    --hosting-base-path "accessible-spatial-computing" # add your repo name later
