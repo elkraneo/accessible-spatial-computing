d7ef76406e2b4b7c9c80fce71d200af43dcf4455
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-07-30 20:46:02 +0200

    init

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "AccessibleSpatialComputing",
+    platforms: [
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AccessibleSpatialComputing",
-            targets: ["AccessibleSpatialComputing"]),
+            name: "A11Y-SC-NYC",
+            targets: ["A11Y-SC-NYC"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AccessibleSpatialComputing"),
-        .testTarget(
-            name: "AccessibleSpatialComputingTests",
-            dependencies: ["AccessibleSpatialComputing"]),
+        .target(name: "A11Y-SC-NYC")
     ]
 )
