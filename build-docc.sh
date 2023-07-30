c1d6f4c8408cad2cabdd545c27856b861cbb6a8f
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-07-30 22:14:50 +0200

    ci :adjust bash command

--- build-docc.sh
+++ build-docc.sh
@@ -5,7 +5,7 @@ xcrun xcodebuild docbuild \
     -destination 'generic/platform=iOS' \
     -derivedDataPath "$PWD/.derivedData"
 
-xcrun docc process-archive transform-for-static-hosting \
-    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/A11Y-SC-NYC.doccarchive" \
-    --output-path ".docs" \
-    --hosting-base-path "accessible-spatial-computing" # add your repo name later
+xcrun docc process-archive \
+    transform-for-static-hosting "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/A11Y-SC-NYC.doccarchive" \
+    --hosting-base-path "accessible-spatial-computing" \
+    --output-path ".docs"
